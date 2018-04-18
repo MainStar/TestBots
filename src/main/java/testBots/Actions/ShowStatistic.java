11077e5ebf42c5a94e307bbe0c645f7cddf9566c
MainStar <vladislav.postoyan@yandex.ru>
2018-04-18 23:57:27 +0300

    Changed show Class

--- src/main/java/testBots/Actions/ShowStatistic.java
+++ src/main/java/testBots/Actions/ShowStatistic.java
@@ -6,6 +6,7 @@ import testBots.dao.entities.LectorsEntity;
 import testBots.dao.service.LectorsService;
 import testBots.dao.utils.HibernateSessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -14,54 +15,75 @@ public class ShowStatistic implements DisplayMode {
     private LectorsService lectorsService = new LectorsService();
     private Session session = HibernateSessionFactory.getSessionFactory().openSession();
     private List<LectorsEntity> lectors;
+    private String departmentName;
+    private String answer = "The average salary of " + departmentName + " is ";
 
-    private final String SHOW_THE_ADVANTAGE = "Show the advantage salary for department";
+    private final String SHOW_THE_ADVANTAGE = "Show the advantage salary for department ";
     private final String SHOW_COUNT = "Show count of employee for";
 
     @Override
     public void displayInfo(String userQuestion) {
 
         lectorsService.setSession(session);
+        lectors = lectorsService.getAllLectors();
 
         if (userQuestion.indexOf(SHOW_THE_ADVANTAGE) != -1){
             showSalary(userQuestion);
         }else if (userQuestion.indexOf(SHOW_COUNT) != -1){
             showCount(userQuestion);
         }else {
-
+            show(userQuestion);
         }
     }
 
     private void show(String userQuestion){
-
+        String[] questionMass = userQuestion.split(" ");
+        for (String el : questionMass){
+            if (!el.equals("Show") && !el.equals("statistic")){
+                departmentName += el + " ";
+            }
+        }
     }
 
     private void showSalary(String userQuestion){
-        int min = 0;
-        int max = 0;
+        List<Integer> maxVal = new ArrayList<>();
         String[] departmentMass = userQuestion.split(SHOW_THE_ADVANTAGE);
-        String departmentName = departmentMass[1];
-        lectors = lectorsService.getAllLectors();
+        departmentName = departmentMass[1];
         for (int i = 0; i < lectors.size(); i++){
             Set<DepartmentEntity> departments = lectors.get(i).getDepartments();
             for (DepartmentEntity el : departments){
-                if (el.getDepartment() == departmentName){
-                    if (el.getSalary() > min){
-                        max = el.getSalary();
-                    }else {
-                        min = el.getSalary();
-                    }
-                }else {
-                    break;
+                if (el.getDepartment().equals(departmentName)) {
+                    maxVal.add(el.getSalary());
                 }
             }
         }
-        System.out.println(min + " " + max);
+        sortList(maxVal);
+        if ((maxVal.size() % 2) == 0){
+            System.out.println(answer + maxVal.get(maxVal.size()/2));
+        }else {
+            System.out.println(answer + maxVal.get((maxVal.size()/2)+1));
+        }
     }
 
     private void showCount(String userQuestion){
         lectors = lectorsService.getAllLectors();
+        String[] mass = userQuestion.split(SHOW_COUNT);
+        departmentName = mass[1];
+        for ()
         System.out.println("Count of employee: " + lectors.size());
+    }
+
+    private List<Integer> sortList(List<Integer> list){
+        for (int i = 0; i < list.size(); i++){
+            for (int x = list.size() - 1; x > i; x-- ){
+                if (list.get(x-1) > list.get(x)){
+                    int k = list.get(x - 1);
+                    list.set(x - 1, list.get(x));
+                    list.set(x, k);
+                }
+            }
+        }
+        return list;
     }
 
     @Override
